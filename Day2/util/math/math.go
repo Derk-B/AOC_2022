2ce7139df50648bca96f671400b8834e562c178b
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2023-10-07 19:35:42 +0200

    Day4 part 1

--- Day2/util/math/math.go
+++ Day2/util/math/math.go
@@ -6,3 +6,17 @@ func Mod(a int, n int) int {
 	return ((a % n) + n) % n
 }
 
+func Min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+	
--- Day4/main.go
+++ Day4/main.go
@@ -3,8 +3,33 @@ package main
 import (
 	"fmt"
 	"Day4/util/fileReader"
+	"strings"
+	"strconv"
 )
 
-func main() {
+func firstPart() {
+	lines := fileReader.ReadLines("input.txt")
 
+	
+	score := 0
+	for _, l := range lines {
+		parts := strings.Split(l, ",")
+		left := strings.Split(parts[0], "-")
+		right := strings.Split(parts[1], "-")
+
+		lStart, _ := strconv.Atoi(left[0])
+		rStart, _ := strconv.Atoi(right[0])
+		lEnd, _ := strconv.Atoi(left[1])
+		rEnd, _ := strconv.Atoi(right[1])
+
+		if lStart >= rStart && lEnd <= rEnd || lStart <= rStart && lEnd >= rEnd {
+			score ++
+		}
+	}
+
+	fmt.Println(score)
+}
+
+func main() {
+	firstPart()
 }
