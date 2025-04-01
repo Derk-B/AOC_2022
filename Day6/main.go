93a9daa1f9cfcb5ce87001a0c6acf92d60f24f77
derk <derk@desktop-derk.home>
2025-04-01 18:42:40 +0200

    Day 6

--- Day6/main.go
+++ Day6/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import "Day6/util/fileReader"
 
+func strIsUnique(str string) bool {
+	m := make(map[rune]bool)
+	for _, c := range str {
+		if m[c] {
+			return false
+		}
+		m[c] = true
+	}
+	return true
+}
+
+func main() {
+	lines := fileReader.ReadLines("input.txt")
+	line := lines[0]
+
+	for i := 3; i < len(line); i++ {
+		if strIsUnique(line[i-3 : i+1]) {
+			println("Answer 1", i+1)
+			break
+		}
+	}
+
+	for i := 13; i < len(line); i++ {
+		if strIsUnique(line[i-13 : i+1]) {
+			println("Answer 2", i+1)
+			break
+		}
+	}
 }
